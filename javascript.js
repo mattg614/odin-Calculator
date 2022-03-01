9ce84c7c4574cfda559eaf75b2b34d999b27613b
Matthew Garza <mattg614@gmail.com>
2022-02-28 20:37:00 -0500

    Add, subtract, multiply, divide and operate fns work

--- javascript.js
+++ javascript.js
@@ -1,19 +1,25 @@
 function add(a,b) {
-
+    return a+b;
 }
 
 function subtract(a,b) {
-
+    return a-b;
 }
 
 function multiply(a,b) {
-
+    return a*b;
 }
 
 function divide(a,b) {
-
+    if (b!==0) return a/b
 }
 
-function operate(a,b) {
-    
+function operate(operator,a,b) {
+    const operands={
+        '+' : add,
+        '-' : subtract,
+        '*' : multiply,
+        '/' : divide
+    }
+    return operands[operator](a,b);
 }
