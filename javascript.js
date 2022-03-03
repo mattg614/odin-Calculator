b99618fbebb9a6a2b0978d70dc7a24a99bb42c29
Matthew Garza <mattg614@gmail.com>
2022-03-03 18:54:39 -0500

    Take in button input and if number display on screen

--- javascript.js
+++ javascript.js
@@ -23,3 +23,27 @@ function operate(operator,a,b) {
     }
     return operands[operator](a,b);
 }
+function numButtonInput(num) {
+    currentNumberStr+=num.textContent;
+    screenText.textContent=currentNumberStr;
+
+}
+//code section to query buttons and perform different actions dependent on which button is pressed
+const screenText=document.querySelector('.screen-nums')
+let currentNumberStr='';
+let previousNumberStr='';
+const num_btn=document.querySelectorAll('button');
+num_btn.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        if(btn.classList.contains('num-button')) {
+            numButtonInput(btn);
+        }
+        else if (btn.classList.contains('operator')) {
+            operatorButtonInput(btn);
+        } else if (btn.id==='decimal') {
+            decimalButtonInput(btn);
+        } else if (btn.id==='clear') {
+            clearButtonInput(btn);
+        }
+    });
+})
